afe476ad6140f5320848bda013bc7db5717b7380
addonisalles <addonisalles001@gmail.com>
2025-08-15 20:54:20 -0300

    Validação de erros, e implementação de funcionalidades
    Foi implementado o código de validação de erros para campos vazios ou inválidos e os recursos de adicionar, excluir, e limpar tudo.
    Evento no txbProduto para adicionar ap teclar ENTER.

--- ListaDeComprasTauste/Form1.Designer.cs
+++ ListaDeComprasTauste/Form1.Designer.cs
@@ -72,10 +72,13 @@
             // 
             // txbProduto
             // 
+            this.txbProduto.BackColor = System.Drawing.SystemColors.Window;
+            this.txbProduto.ForeColor = System.Drawing.Color.Black;
             this.txbProduto.Location = new System.Drawing.Point(243, 182);
             this.txbProduto.Name = "txbProduto";
             this.txbProduto.Size = new System.Drawing.Size(190, 23);
             this.txbProduto.TabIndex = 3;
+            this.txbProduto.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txbProduto_KeyDown);
             // 
             // btnAdicionar
             // 
@@ -85,6 +88,7 @@
             this.btnAdicionar.TabIndex = 4;
             this.btnAdicionar.Text = "Adicionar";
             this.btnAdicionar.UseVisualStyleBackColor = true;
+            this.btnAdicionar.Click += new System.EventHandler(this.btnAdicionar_Click);
             // 
             // libCompras
             // 
@@ -103,6 +107,7 @@
             this.btnExcluir.TabIndex = 6;
             this.btnExcluir.Text = "Excluir";
             this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
             // 
             // btnLimpar
             // 
@@ -112,6 +117,7 @@
             this.btnLimpar.TabIndex = 7;
             this.btnLimpar.Text = "Limpar Lista";
             this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
             // 
             // lbCompras
             // 
@@ -130,7 +136,7 @@
             this.Font = new System.Drawing.Font("Schadow BT", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "lbCompras";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
